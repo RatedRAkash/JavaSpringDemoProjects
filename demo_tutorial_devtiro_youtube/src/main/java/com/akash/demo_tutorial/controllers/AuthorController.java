89f79dfb82188b9112da02d35a343146416f6fb6
AnantaAkashPodder <anantaakash.podder@gmail.com>
2023-11-08 19:10:22 +0600

    Create Author Endpoint + Test Author Create Endpoint Implemented

--- demo_tutorial_devtiro_youtube/src/main/java/com/akash/demo_tutorial/controllers/AuthorController.java
+++ demo_tutorial_devtiro_youtube/src/main/java/com/akash/demo_tutorial/controllers/AuthorController.java
@@ -4,6 +4,8 @@ import com.akash.demo_tutorial.domain.dto.AuthorDto;
 import com.akash.demo_tutorial.domain.entities.AuthorEntity;
 import com.akash.demo_tutorial.mappers.CustomObjectMapper;
 import com.akash.demo_tutorial.services.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +22,14 @@ public class AuthorController {
     }
 
     @PostMapping(path = "/authors")
-    public AuthorDto createAuthor(@RequestBody AuthorDto authorDto){
+    public ResponseEntity<AuthorDto> createAuthor(@RequestBody AuthorDto authorDto){
         AuthorEntity authorEntity = authorMapper.mapFrom(authorDto);
         AuthorEntity savedAuthorEntity = authorService.save(authorEntity);
-        return authorMapper.mapTo(savedAuthorEntity);
+
+
+//        ResponseEntity ---> Response ee Custom STATUS CODE pathanor jonno amra eita Use kori...
+//        eikane jemon CREATED er por 201 HTTP CODE Return korbe.. naile Normally use korle 200 Return kore
+        return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
     }
 
 }
--- demo_tutorial_devtiro_youtube/src/test/java/com/akash/demo_tutorial/dao/impl/BookEntityDaoImplTests.java
+++ demo_tutorial_devtiro_youtube/src/test/java/com/akash/demo_tutorial/dao/impl/BookEntityDaoImplTests.java
@@ -1,41 +1,41 @@
-package com.akash.demo_tutorial.dao.impl;
-
-import com.akash.demo_tutorial.domain.entities.BookEntity;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-
-@ExtendWith(MockitoExtension.class)
-public class BookEntityDaoImplTests {
-
-    @Mock
-    private JdbcTemplate jdbcTemplate;
-
-    @InjectMocks
-    private BookDaoImpl underTest;  // BookDaoImpl er moddhe jei Constructor ase... sheikane upor er "@Mock" diye JdbcTemplate amra Auto Inject korte parbo
-
-    @Test
-    public void testThatCreateBookGeneratesCorrectSql() {
-
-        BookEntity book = BookEntity.builder()
-                .isbn("978-1-2345-6789-0")
-                .title("The Best Book in the World")
-                .authorId(1L)
-                .build();
-
-        underTest.create(book);
-
-        verify(jdbcTemplate).update(
-                eq("INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)"),
-                eq("978-1-2345-6789-0"),
-                eq("The Best Book in the World"),
-                eq(1L)
-        );
-    }
-}
+//package com.akash.demo_tutorial.dao.impl;
+//
+//import com.akash.demo_tutorial.domain.entities.BookEntity;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//
+//import static org.mockito.ArgumentMatchers.eq;
+//import static org.mockito.Mockito.verify;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class BookEntityDaoImplTests {
+//
+//    @Mock
+//    private JdbcTemplate jdbcTemplate;
+//
+//    @InjectMocks
+//    private BookDaoImpl underTest;  // BookDaoImpl er moddhe jei Constructor ase... sheikane upor er "@Mock" diye JdbcTemplate amra Auto Inject korte parbo
+//
+//    @Test
+//    public void testThatCreateBookGeneratesCorrectSql() {
+//
+//        BookEntity book = BookEntity.builder()
+//                .isbn("978-1-2345-6789-0")
+//                .title("The Best Book in the World")
+//                .authorId(1L)
+//                .build();
+//
+//        underTest.create(book);
+//
+//        verify(jdbcTemplate).update(
+//                eq("INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)"),
+//                eq("978-1-2345-6789-0"),
+//                eq("The Best Book in the World"),
+//                eq(1L)
+//        );
+//    }
+//}
