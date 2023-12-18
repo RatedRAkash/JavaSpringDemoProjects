f86be3174c352058bbdbdcc998a1ba490ad5541c
AnantaAkashPodder <anantaakash.podder@gmail.com>
2023-12-18 21:06:35 +0600

    rabbitMQ Spring Boot Demo Project Added

---
+++ akash_rabbitmq_spring_boot/src/main/java/com/tutorial/akash_rabbitmq_spring_boot/AkashRabbitmqSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.akash_rabbitmq_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkashRabbitmqSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkashRabbitmqSpringBootApplication.class, args);
+	}
+
+}
---
+++ akash_rabbitmq_spring_boot/src/test/java/com/tutorial/akash_rabbitmq_spring_boot/AkashRabbitmqSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.akash_rabbitmq_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkashRabbitmqSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
