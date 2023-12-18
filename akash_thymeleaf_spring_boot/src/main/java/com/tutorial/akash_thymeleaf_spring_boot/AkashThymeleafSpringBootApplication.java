0423b8e962a548a8801464e8c2d860feca21b9f5
AnantaAkashPodder <anantaakash.podder@gmail.com>
2023-12-18 19:11:48 +0600

    akash_thymeleaf_spring_project ADDED and Removed reduntant project of spring-master-class of Amigoscode

---
+++ akash_thymeleaf_spring_boot/src/main/java/com/tutorial/akash_thymeleaf_spring_boot/AkashThymeleafSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.akash_thymeleaf_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkashThymeleafSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkashThymeleafSpringBootApplication.class, args);
+	}
+
+}
---
+++ akash_thymeleaf_spring_boot/src/test/java/com/tutorial/akash_thymeleaf_spring_boot/AkashThymeleafSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.akash_thymeleaf_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkashThymeleafSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
