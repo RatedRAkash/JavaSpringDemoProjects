eb8ab1f0b7e689f4229f406e5494fd917c808bf6
AnantaAkashPodder <anantaakash.podder@gmail.com>
2023-12-26 13:06:48 +0600

    wso2_dummy: if Consumer is Down, when it will come up, it will still Consume Message

--- wso2_dummy/src/main/java/com/psl/wso2_dummy/wso2/np/handler/HandlerTxnStatement.java
+++ wso2_dummy/src/main/java/com/psl/wso2_dummy/wso2/np/handler/HandlerTxnStatement.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 @EnableBinding(TxnTopicProcessor.class)
 public class HandlerTxnStatement {
 
-    @StreamListener(target = TxnTopicProcessor.TXN_TOPIC)
+    @StreamListener(target = TxnTopicProcessor.TXN_TOPIC_INPUT)
     public void consumer1(TxnType txnType) {
         System.out.println("Consumer1 SEND_MONEY handled: " + txnType.toString());
     }
