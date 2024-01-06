647eaf00d80055b6b3ac6ad935c36079f1c53eb8
AnantaAkashPodder <anantaakash.podder@gmail.com>
2024-01-06 11:46:43 +0600

    akash_spring_boot_jdbc_tutorial PROJECT added & REMOVED gradle_project_tutorial Project

---
+++ akash_spring_boot_jdbc_tutorial/src/main/java/com/tutorial/akash_spring_boot_jdbc_tutorial/AkashSpringBootJdbcTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.akash_spring_boot_jdbc_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkashSpringBootJdbcTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkashSpringBootJdbcTutorialApplication.class, args);
+	}
+
+}
