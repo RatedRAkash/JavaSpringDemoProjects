f826f7a90065638aa6d892e6dd2704a0c92493d2
AnantaAkashPodder <anantaakash.podder@gmail.com>
2024-01-12 16:37:40 +0600

    e_learniverse_spring_boot PROJECT: added Firebase FCM Dependency called firebase-admin

--- e_learniverse_spring_boot/src/main/java/com/akash/e_learniverse_spring_boot/ELearniverseSpringBootApplication.java
+++ e_learniverse_spring_boot/src/main/java/com/akash/e_learniverse_spring_boot/ELearniverseSpringBootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ELearniverseSpringBootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ELearniverseSpringBootApplication.class, args);
 	}
-
 }
