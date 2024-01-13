4d0d708f4d40c34a2169fcd1af648de17819d5e5
AnantaAkashPodder <anantaakash.podder@gmail.com>
2024-01-13 12:35:28 +0600

    E_Learniverse Spring Boot: added Thymeleaf Templates & also HomeController

--- e_learniverse_spring_boot/src/main/java/com/akash/e_learniverse_spring_boot/controller/rest_controller/MainRestController.java
+++ e_learniverse_spring_boot/src/main/java/com/akash/e_learniverse_spring_boot/controller/rest_controller/MainRestController.java
@@ -1,19 +1,18 @@
-package com.akash.e_learniverse_spring_boot.controller;
+package com.akash.e_learniverse_spring_boot.controller.rest_controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("api/")
-public class MainController {
+public class MainRestController {
 
-    private static final Logger logger = LogManager.getLogger(MainController.class);
+    private static final Logger logger = LogManager.getLogger(MainRestController.class);
 
 
     @GetMapping("/home")
