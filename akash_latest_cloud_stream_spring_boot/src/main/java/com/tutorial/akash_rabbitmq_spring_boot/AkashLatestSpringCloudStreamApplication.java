a8e40cb584af8584d92acfe3286eca4c67b55007
AnantaAkashPodder <akash@surecash.net>
2024-01-20 17:49:25 +0600

    akash_latest_cloud_stream_spring_boot PROJECT: Producer, Processor & Consumer Code+Configuration in application.yml added

--- akash_latest_cloud_stream_spring_boot/src/main/java/com/tutorial/akash_rabbitmq_spring_boot/AkashLatestSpringCloudStreamApplication.java
+++ akash_latest_cloud_stream_spring_boot/src/main/java/com/tutorial/akash_rabbitmq_spring_boot/AkashLatestSpringCloudStreamApplication.java
@@ -3,8 +3,10 @@ package com.tutorial.akash_rabbitmq_spring_boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
+
 
 @SpringBootApplication
 public class AkashLatestSpringCloudStreamApplication {
@@ -13,12 +15,29 @@ public class AkashLatestSpringCloudStreamApplication {
 		SpringApplication.run(AkashLatestSpringCloudStreamApplication.class, args);
 	}
 
+
+	//Producer
 	@Bean
-	public Function<String, String> toUpperCase(){
+	public Supplier<String> myProducer() {
+		return () -> "sergio ramos";
+	}
+
+
+	//Processor
+	@Bean
+	public Function<String, String> toUpperCase() {
 		return (str) -> {
 			str = str.toUpperCase();
-			System.out.println(str);
 			return str;
+		};
+	}
+
+	
+	//Consumer
+	@Bean
+	public Consumer<String> myConsumer() {
+		return (str) -> {
+			System.out.println(str);
 		};
 	}
 }
